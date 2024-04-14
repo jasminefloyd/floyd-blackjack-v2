0dcba4b7a8257ae77b19fec4eb72246d8686c3b6
jasminefloyd <42697356+jasminefloyd@users.noreply.github.com>
2024-04-13 23:33:07 -0400

    index updates

--- assets/index.js
+++ assets/index.js
@@ -3,14 +3,8 @@ const player = {
     name: "",
     chips: 0,
     cards: [],
-    sum: 0
-}
-
-const dealer = {
-    name: "",
-    chips: 0,
-    cards: [],
-    sum: 0
+    sum: 0,
+    currentBid: 0
 }
 
 //Global Variables
@@ -32,8 +26,39 @@ var dealerGameMessage_El = document.getElementById("dealerGameMessage")
 var playBoard_El = document.getElementById("playBoard")
 var gameBoard_El = document.getElementById("gameBoard")
 var startBoard_El = document.getElementById("startBoard")
+var rePlay_El = document.getElementById("rePlay")
+var playerChipsLabel_El = document.getElementById("playerChipsLabel")
+var playerNameLabel_El = document.getElementById("playerNameLabel")
+var playerBidLabel_El = document.getElementById("playerCurrentBid")
 
 //Functions
+
+
+// function resetGameBoard(){
+//     playBoard_El.style.display = 
+//     startBoard_El.style.display = 
+//     gameBoard_El.style.display = 
+
+// }
+
+function resetGame(){
+    player.name = document.getElementById("playerName").value
+    player.currentBid = document.getElementById("playerBet").value
+    player.chips = 300
+    playerNameLabel_El.textContent = "Name: " + player.name
+    playerBidLabel_El.textContent = "Current Bid: " + player.currentBid
+    playerChipsLabel_El.textContent = "Remaining Chips: " + player.chips
+}
+
+
+function playGame(){
+    // resetGameBoard()
+    // resetGame()
+    dealPlayerCards()
+    checkBlackJack()
+}
+
+
 function dealPlayerCards(){
     firstCard = (Math.floor(Math.random() * 11) + 1)
     secondCard = (Math.floor(Math.random() * 11) + 1)
@@ -44,39 +69,40 @@ function dealPlayerCards(){
     playerCards_El.textContent = "Cards: " + (player.cards[0]) + ", " + (player.cards[1])
 }
 
-
-function playGame(){
-    player.name = document.getElementById("playerName").value
-    player.chips = 300
-    startBoard_El.style.display = `none`
-    playBoard_El.style.display = `block`
-
-}
-
-
-
-function dealCards(){
-    playBoard_El.style.display = `none`
-    gameBoard_El.style.display = `block`
-    dealPlayerCards()
-    checkBlackJack()
-}
-
-
 function playerHit(){
     card = (Math.floor(Math.random() * 11) + 1)
     player.cards.push(card)
     player.sum += card
     playerCounter_El.textContent = "Total: " + player.sum
     playerCards_El.textContent = "Cards: " + (player.cards.join(", "))
-    checkBlackJack()
+}
+
+function playerStay(){
+    playerGameMessage_El.textContent = "Stay. You have " + player.sum
+    hitBtn_El.style.visibility = `hidden`;
+    stayBtn_El.style.visibility = `hidden`;
+    rePlay_El.style.visibility = `visible`;
+
 }
 
 function checkBlackJack(){
     if (player.sum === 21) {
         playerGameMessage_El.textContent = "Blackjack! You win!";
-        
-    } else {
+        hitBtn_El.style.visibility = `hidden`;
+        stayBtn_El.style.visibility = `hidden`;
+        rePlay_El.style.visibility = `visible`;
+
+    } else if (player.sum > 21) {
+        playerGameMessage_El.textContent = "Busted! Do you want to play again!";
+        hitBtn_El.style.visibility = `hidden`;
+        stayBtn_El.style.visibility = `hidden`;
+        rePlay_El.style.visibility = `visible`;
+
+    }
+    
+    else {
         playerGameMessage_El.textContent = "Would you like to hit or stay?";
     }
+
 }
+
